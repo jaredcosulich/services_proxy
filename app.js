47c78582997bc41df9999de219133f2bc7978af4
Jared Cosulich <jared.cosulich@gmail.com>
2022-03-14 19:18:41 -0400

    switching to express with cors

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const http = require('http');
+const express = require('express');
+const cors = require('cors');
 const httpProxy = require('http-proxy');
- 
-const proxy = httpProxy.createProxyServer({});
- 
-const server = http.createServer((req, res) => {
-  if (req.url.startsWith('/api')) {
-    proxy.web(req, res, { target: 'http://privatehostedganache:10000' });
-  } else {
-    proxy.web(req, res, { target: 'http://privatehostedganache:8545' });
-  }
+
+const appPort = process.env.PORT || 8000; 
+
+const app = express()
+app.use(cors())
+
+app.all("*", (req, res, next) => {
+  console.log("REQUEST", req.url); // do anything you want here
+  next();
 });
- 
-console.log("listening on port 8000")
-server.listen(8000, '0.0.0.0');
+
+const proxy = httpProxy.createProxyServer({});
+
+app.get('/api/*', (req, res) => {
+  proxy.web(req, res, { target: 'http://privatehostedganache:10000' });
+})
+
+app.get('/', (req, res) => {
+  proxy.web(req, res, { target: 'http://privatehostedganache:8545' });
+})
+
+app.listen(appPort, '0.0.0.0', () => console.log(`App listening on port ${appPort}...`))
