22028e5439da79e12f75650fdff669e57c0b830a
Jared Cosulich <jared.cosulich@gmail.com>
2022-03-14 20:24:47 -0400

    receive posts and gets

--- app.js
+++ app.js
@@ -7,18 +7,18 @@ const appPort = process.env.PORT || 8000;
 const app = express()
 app.use(cors())
 
-app.all("*", (req, res, next) => {
-  console.log("REQUEST", req.url); // do anything you want here
-  next();
-});
+// app.all("*", (req, res, next) => {
+//   console.log("REQUEST", req.url); 
+//   next();
+// });
 
 const proxy = httpProxy.createProxyServer({});
 
-app.get('/api/*', (req, res) => {
+app.all('/api/*', (req, res) => {
   proxy.web(req, res, { target: 'http://privatehostedganache:10000' });
 })
 
-app.get('/', (req, res) => {
+app.all('/', (req, res) => {
   proxy.web(req, res, { target: 'http://privatehostedganache:8545' });
 })
 
